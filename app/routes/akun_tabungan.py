50e128c70d4addc2fa43684cb4ba5de0f685483a
Upi PC <wajibro@yahoo.com>
2026-08-26 21:58:11 +0700

    Menambahkan fitur edit dan transfer akun tabungan

--- app/routes/akun_tabungan.py
+++ app/routes/akun_tabungan.py
@@ -28,10 +28,24 @@ def akun_tabungan():
     tab = request.args.get('tab', '')
     message = request.args.get('message', '')
 
+    edit_akun = None
+    edit_akun_id = request.args.get('edit_akun_id')
+    if edit_akun_id:
+        response = supabase.table('akun_tabungan').select('*').eq('id', edit_akun_id).execute()
+        if response.data:
+            edit_akun = response.data[0]
+
+    transfer_akun = None
+    transfer_akun_id = request.args.get('transfer_akun_id')
+    if transfer_akun_id:
+        response = supabase.table('akun_tabungan').select('*').eq('id', transfer_akun_id).execute()
+        if response.data:
+            transfer_akun = response.data[0]
+
     response = supabase.table('akun_tabungan').select('*').order('id', desc=False).execute()
     data_akun_tabungan = response.data
 
-    return render_template('akun_tabungan.html', total_saldo=total_saldo, data_akun_tabungan=data_akun_tabungan, tab=tab, message=message)
+    return render_template('akun_tabungan.html', total_saldo=total_saldo, data_akun_tabungan=data_akun_tabungan, tab=tab, message=message, edit_akun=edit_akun, transfer_akun=transfer_akun)
 
 @akun_tabungan_bp.route('/tambah_akun', methods=['GET', 'POST'])
 @login_required
@@ -53,22 +67,81 @@ def tambah_akun():
                 "nama_akun": nama_akun,
                 "total": total
             }).execute()
-
-        if total != 0:
-            supabase.table('data_historis')\
-                .insert({
-                    "tanggal"           : date.today().isoformat(),
-                    "nama_akun"         : nama_akun,
-                    "jenis"             : "Tambah Akun",
-                    "kategori"          : "Tambah Akun",
-                    "total_perubahan"   : total,
-                    "total_akhir"       : total
-                }).execute()
         return redirect(url_for('akun_tabungan.akun_tabungan', tab='', message=''))        
 
-@akun_tabungan_bp.route('/hapus_akun/<nama_akun>', methods=['POST'])
+@akun_tabungan_bp.route('/edit_akun/<id>', methods=['GET', 'POST'])
 @login_required
-def delete(nama_akun):
-    supabase.table('akun_tabungan').delete().eq('nama_akun', nama_akun).execute()
+def edit_form(id):
+    return redirect(url_for('akun_tabungan.akun_tabungan', edit_akun_id=id))
+
+@akun_tabungan_bp.route('/update/<id>', methods=['POST'])
+@login_required
+def update(id):
+    input_nama_akun_baru = request.form.get('input_nama_akun_baru')
+    input_total_akun_baru = request.form.get('input_total_akun_baru')
+
+    response = supabase.table('akun_tabungan').select('*').eq('nama_akun', input_nama_akun_baru).neq('id', id).execute()
+
+    if response.data:
+        return redirect(url_for('akun_tabungan.akun_tabungan', edit_akun=id, tab='edit_akun', message='Sudah ada nama akun tabungan yang sama, gunakan nama lain'))
+    
+    supabase.table('akun_tabungan')\
+        .update({
+            "nama_akun": input_nama_akun_baru,
+            "total": input_total_akun_baru
+        }).eq('id', id).execute()
+    
+    session.pop('edit_akun_data', None)
+    session.pop('edit_akun_id', None)
+
+    return redirect(url_for('akun_tabungan.akun_tabungan', tab='', message=''))
+
+@akun_tabungan_bp.route('/transfer_akun_form/<id>', methods=['GET', 'POST'])
+@login_required
+def transfer_form(id):
+    return redirect(url_for('akun_tabungan.akun_tabungan', transfer_akun_id=id))
+
+@akun_tabungan_bp.route('/transfer_akun/<id>', methods=['GET', 'POST'])
+@login_required
+def transfer(id):
+    target_transfer = request.form.get('target_transfer')
+    input_total_transfer = request.form.get('input_total_transfer')
+    total_transfer = float(input_total_transfer)
+
+    response = supabase.table('akun_tabungan').select('*').eq('id', id).execute()
+    data_sumber = response.data[0]
+    total_sumber = float(data_sumber['total'])
+    total_sumber -= total_transfer
+
+    response = supabase.table('akun_tabungan').select('*').eq('nama_akun', target_transfer).execute()
+    data_target = response.data[0]
+    total_target = float(data_target['total'])
+    total_target += total_transfer
+
+    supabase.table('akun_tabungan')\
+        .upsert({
+            'id': id,
+            'total': total_sumber
+        }).execute()
+    supabase.table('akun_tabungan')\
+        .upsert({
+            'nama_akun': target_transfer,
+            'total': total_sumber
+        }).execute()
+
+    session.pop('transfer_akun_data', None)
+    session.pop('transfer_akun_id', None)
+
+    return redirect(url_for('akun_tabungan.akun_tabungan', tab='', message=''))
+
+@akun_tabungan_bp.route('/hapus_akun/<id>', methods=['POST'])
+@login_required
+def delete(id):
+    response = supabase.table('akun_tabungan').select('*').eq('id', id).execute()
+    data = response.data[0]
+    nama_akun = data['nama_akun']
+
     supabase.table('data_historis').delete().eq('nama_akun', nama_akun).eq('jenis', "Tambah Akun").execute()
+    supabase.table('akun_tabungan').delete().eq('id', id).execute()
+
     return redirect(url_for('akun_tabungan.akun_tabungan', tab='', message=''))
